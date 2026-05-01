1d3a3fd12f1c97e30ade066ac0f76c199082ac64
anuj9631 <anuj98426@gmail.com>
2026-05-01 16:47:20 +0530

    added note inut

--- components/NoteInput.tsx
+++ components/NoteInput.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { SourceType } from "@/lib/supabase";
-
+import VoiceInput from "@/components/VoiceInput";
 const TYPES: {
   value: SourceType;
   emoji: string;
